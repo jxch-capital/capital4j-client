d90a5556ddbb58854004f2003270b5765d8cf41d
jxch <qianbuhan@proton.me>
2024-04-29 21:36:02 +0800

    移除Neo4J

--- src/main/java/org/jxch/capital/client/config/DBConfig.java
+++ src/main/java/org/jxch/capital/client/config/DBConfig.java
@@ -1,7 +1,6 @@
 package org.jxch.capital.client.config;
 
 import jakarta.persistence.EntityManagerFactory;
-import org.neo4j.driver.Driver;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -9,8 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,10 +17,8 @@ import javax.sql.DataSource;
 @EnableTransactionManagement
 @EntityScan(basePackages = {"org.jxch.capital.client.db.h2.po"})
 @EnableJpaRepositories(basePackages = {"org.jxch.capital.client.db.h2.dao"})
-@EnableNeo4jRepositories(basePackages = {"org.jxch.capital.client.db.neo4j.dao"})
 public class DBConfig {
     public static final String TRANSACTION_MANAGER = "transactionManager";
-    public static final String TRANSACTION_MANAGER_NEO4J = "neo4jTransactionManager";
 
     @Bean
     @Primary
@@ -36,11 +31,6 @@ public class DBConfig {
     @Bean(TRANSACTION_MANAGER)
     public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         return new JpaTransactionManager(entityManagerFactory);
-    }
-
-    @Bean(TRANSACTION_MANAGER_NEO4J)
-    public Neo4jTransactionManager neo4jTransactionManager(Driver driver) {
-        return new Neo4jTransactionManager(driver);
     }
 
 }
